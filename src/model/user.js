39277098f14ba6d4ab36874677cc129188b5128b
umutplvn <umutpehlivan2078@gmail.com>
2023-12-21 00:52:21 +0000

    up

--- src/model/user.js
+++ src/model/user.js
@@ -1,16 +1,25 @@
 "use strict"
 const mongoose =require('mongoose')
-
+const passwordEncrypt = require('../helpers/passwordEncrypt')
 const userSchema = new mongoose.Schema({
 
-userName:{
+email:{
     type:String,
     trim:true,
+    unique:true,
+    required:true,
+    validate:[ 
+        (email)=> (email.includes('@') && email.includes('.')), 'Email is not valid'
+        // (email)=>{ return (email.indexOf('@' && email.indexOf('.')))}, 'Email is not valid'
+    ]
 },
 
 password:{
     type:String,
     trim:true,
+    required:true,
+    set:(password)=>passwordEncrypt(password)
+
 }
 
 },
