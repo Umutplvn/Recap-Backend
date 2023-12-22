127a5c66852c049acd21835b639d7e7073ab8e0d
umutplvn <umutpehlivan2078@gmail.com>
2023-12-22 05:09:47 +0000

    up

--- src/routes/category.js
+++ src/routes/category.js
@@ -2,9 +2,10 @@
 
 const router=require('express').Router()
 const Category = require('../controller/category')
+const permissions=require('../middlewares/permissions')
 
 router.route('/')
-.get(Category.list)
+.get(permissions.isLogin, Category.list)
 .post(Category.create)
 
 router.route('/:categoryId')
